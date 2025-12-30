2f738e76455665269cd86a3e9f5031e2a2c59d42
Dhileep Kumar <dhileepkumar441@gmail.com>
2025-12-30 14:53:43 +0530

    second commit

--- service/apiClient.ts
+++ service/apiClient.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://paste-bin-api.o
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
